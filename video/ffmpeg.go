a23e826f6b4a746fe79146631c7d3654c446dfaf
Michał Meyer <mmey3r.dev@gmail.com>
2024-02-27 22:22:40 +0100

    d

--- video/ffmpeg.go
+++ video/ffmpeg.go
@@ -11,14 +11,14 @@ import (
 func StartFFMPEGVideoStreaming() error {
 	cmd := exec.Command("ffmpeg", "-f", "v4l2", "-framerate", "30", "-re", "-stream_loop", "-1", "-video_size", "1920x1080", "-input_format", "mjpeg", "-i", os.Getenv("CAMERA_DEVICE_PATH"), "-c", "copy", "-f", "rtsp", os.Getenv("SERVER_ADDRESS"))
 
-	logFile, err := os.Create(fmt.Sprintf("logs/%d.txt", time.Now().Unix()))
+	logFile, err := os.Create(fmt.Sprintf("logs/%s.txt", time.Now()))
 	if err != nil {
 		return err
 	}
 
 	cmd.Stdout = logFile
 	cmd.Stderr = logFile
-	return cmd.Run()
+	return cmd.Start()
 }
 
 func StopFFMPEGVideoStreaming() error {
