3efe58261aeaadeecf14f888f42c7f719b81ec83
Michał Meyer <mmey3r.dev@gmail.com>
2024-04-10 21:13:20 +0200

    Add websocket server

--- main.go
+++ main.go
@@ -9,16 +9,17 @@ import (
 )
 
 
-func main(){
+func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error loading .env file: %s", err)
 	}
-	err = server.RunServer(
-		os.Getenv("PORT"),
-		os.Getenv("CERT_PATH"),
-		os.Getenv("KEY_PATH"),
-	)
+	// err = server.RunWebTransportServer(
+		// os.Getenv("PORT"),
+		// os.Getenv("CERT_PATH"),
+		// os.Getenv("KEY_PATH"),
+	// )
+	err = server.RunWebSocketServer(os.Getenv("PORT"))
 	if err != nil {
 		log.Fatalf("Failed to start server: %s", err)
 	}
