b74e2c3da15d178d77e6e74fd2152d5826e739b1
Marcel Dybowski <mdybowski20@gmail.com>
2024-07-24 19:08:46 +0200

    adding range and boxes

--- gui/gui.py
+++ gui/gui.py
@@ -11,7 +11,9 @@ class RobotGUI:
         self.send_xyz_button = None
         self.send_joints_button = None
         self.right_sliders = None
+        self.right_entries = None
         self.left_sliders = None
+        self.left_entries = None
 
         self.root = root
         self.root.title("Wariat Robot - GUI")
@@ -31,7 +33,7 @@ class RobotGUI:
 
         self.video_label = ttk.Label(self.video_frame)
         self.video_label.grid(row=0, column=0)
-        self.cap = cv2.VideoCapture(0)  # mozna tu dac stream RTSP
+        self.cap = cv2.VideoCapture(0)  # można tu dać stream RTSP
         self.update_video_frame()
 
         self.websocket_client = WsClient("ws://localhost:8765")
@@ -41,16 +43,35 @@ class RobotGUI:
 
     def create_left_panel(self) -> None:
         self.left_sliders = []
-        for i in range(6):
+        self.left_entries = []
+        slider_ranges = [
+            (0, 360),  # Range for Joint 0
+            (-95, 110),  # Range for Joint 1
+            (-90, 130),  # Range for Joint 2
+            (0, 360),  # Range for Joint 3
+            (-100, 100)  # Range for Joint 4
+        ]
+
+        for i, (min_val, max_val) in enumerate(slider_ranges):
+            frame = ttk.Frame(self.left_frame)
+            frame.grid(row=i, column=0, padx=5, pady=5)
+
             slider = tk.Scale(
-                self.left_frame,
-                from_=0,
-                to=100,
+                frame,
+                from_=min_val,
+                to=max_val,
                 orient="horizontal",
+                length=300,
                 label=f"Joint {i}",
+                command=lambda value, i=i: self.update_entry_from_slider(value, i, 'left')
             )
-            slider.grid(row=i, column=0, padx=5, pady=5)
+            slider.grid(row=0, column=0, padx=5, pady=5)
             self.left_sliders.append(slider)
+
+            entry = ttk.Entry(frame)
+            entry.grid(row=0, column=1, padx=5, pady=5)
+            entry.bind('<Return>', lambda event, i=i: self.update_slider_from_entry(event, i, 'left'))
+            self.left_entries.append(entry)
 
         self.send_joints_button = ttk.Button(
             self.left_frame, text="Send commands", command=self.send_joints_commands
@@ -59,16 +80,33 @@ class RobotGUI:
 
     def create_right_panel(self) -> None:
         self.right_sliders = []
-        for i in range(3):
+        self.right_entries = []
+        slider_ranges = [
+            (0, 100),  # Range for Slider 1
+            (0, 100),  # Range for Slider 2
+            (0, 100)  # Range for Slider 3
+        ]
+
+        for i, (min_val, max_val) in enumerate(slider_ranges):
+            frame = ttk.Frame(self.right_frame)
+            frame.grid(row=i, column=0, padx=5, pady=5)
+
             slider = tk.Scale(
-                self.right_frame,
-                from_=0,
-                to=100,
+                frame,
+                from_=min_val,
+                to=max_val,
                 orient="horizontal",
+                length=300,
                 label=f"Slider {i + 1}",
+                command=lambda value, i=i: self.update_entry_from_slider(value, i, 'right')
             )
-            slider.grid(row=i, column=0, padx=5, pady=5)
+            slider.grid(row=0, column=0, padx=5, pady=5)
             self.right_sliders.append(slider)
+
+            entry = ttk.Entry(frame)
+            entry.grid(row=0, column=1, padx=5, pady=5)
+            entry.bind('<Return>', lambda event, i=i: self.update_slider_from_entry(event, i, 'right'))
+            self.right_entries.append(entry)
 
         self.gripper_button = ttk.Button(
             self.right_frame, text=self.get_gripper_text(), command=self.toggle_gripper
@@ -79,6 +117,22 @@ class RobotGUI:
             self.right_frame, text="Send commands", command=self.send_xyz_commands
         )
         self.send_xyz_button.grid(row=4, column=0, padx=5, pady=5)
+
+    def update_entry_from_slider(self, value, index, side):
+        if side == 'left':
+            self.left_entries[index].delete(0, tk.END)
+            self.left_entries[index].insert(0, str(value))
+        else:
+            self.right_entries[index].delete(0, tk.END)
+            self.right_entries[index].insert(0, str(value))
+
+    def update_slider_from_entry(self, event, index, side):
+        if side == 'left':
+            value = self.left_entries[index].get()
+            self.left_sliders[index].set(value)
+        else:
+            value = self.right_entries[index].get()
+            self.right_sliders[index].set(value)
 
     def get_gripper_text(self) -> str:
         return "Open Gripper" if not self.gripper_status else "Close Gripper"
@@ -98,9 +152,6 @@ class RobotGUI:
     def update_video_frame(self) -> None:
         ret, frame = self.cap.read()
         if ret:
-            # scale_percent = 50
-            # width = int(frame.shape[1] * scale_percent / 100)
-            # height = int(frame.shape[0] * scale_percent / 100)
             width = 600
             height = 400
             dim = (width, height)
@@ -108,7 +159,6 @@ class RobotGUI:
             resized_frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
 
             frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             img = Image.fromarray(frame)
             imgtk = ImageTk.PhotoImage(image=img)
             self.video_label.imgtk = imgtk
