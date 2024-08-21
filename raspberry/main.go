fa60be71a969e17cedd25f3f5e96ac22f7426f02
Michał Meyer <mmey3r.dev@gmail.com>
2024-08-21 17:31:17 +0200

    make robot instance global

--- raspberry/main.go
+++ raspberry/main.go
@@ -4,16 +4,30 @@ import (
 	"log"
 	"os"
 
+	"github.com/xTaube/vr-controlled-robot-arm/robot"
 	"github.com/xTaube/vr-controlled-robot-arm/server"
+	"go.bug.st/serial"
 )
 
 func main() {
-	// err = server.RunWebTransportServer(
-	// os.Getenv("PORT"),
-	// os.Getenv("CERT_PATH"),
-	// os.Getenv("KEY_PATH"),
-	// )
-	err := server.RunWebSocketServer(os.Getenv("PORT"))
+	log.Println("Initializing robot arm...")
+	robot, err := robot.InitRobot(
+		robot.UartConfig{
+			PortName: os.Getenv("UART_PORT"),
+			Parity:   serial.EvenParity,
+			StopBits: serial.OneStopBit,
+			BaudRate: 115200,
+			DataBits: 8,
+		},
+	)
+	if err != nil {
+		log.Printf("Error initializing robot arm: %s.\n", err)
+		return
+	}
+	defer robot.ShutDown()
+	log.Println("Robot arm initialized.")
+
+	err = server.RunWebSocketServer(os.Getenv("PORT"), robot)
 	if err != nil {
 		log.Fatalf("Failed to start server: %s", err)
 	}
